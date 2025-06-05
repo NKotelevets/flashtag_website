68b36489dcc556415e76b67cefe336a621afcf18
Nikolay-Kotelevets <nikolay.kotelevets@scrumlaunch.com>
2025-06-06 00:23:27 +0300

    failed build

--- src/pages/privacy-policy.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PrivacyPolicy: React.FC = () => {
-  return <div>Privacy Policy</div>;
-};
-
-export default PrivacyPolicy;
--- src/pages/terms-of-service.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const TermsOfService: React.FC = () => {
-  return <div>Terms of Service</div>;
-};
-
-export default TermsOfService;
